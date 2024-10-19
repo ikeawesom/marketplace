af80898d77ab0003fd9a23e426dd9983e1445c77
ikeawesom <ikelimjl@gmail.com>
2024-10-19 15:13:18 +0800

    added filter header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { HeaderFilterComponent } from '../filtering/header/header-filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeaderFilterComponent],
 })
 export class AppComponent {
   title = 'marketplace';
