691ec89c518618720c07fa1c6a2303efcae5cffb
ikeawesom <ikelimjl@gmail.com>
2024-10-19 13:46:06 +0800

    created top nav

---
+++ src/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ProfileComponent } from '../profile/profile.component';
+
+@Component({
+  selector: 'header-component',
+  standalone: true,
+  templateUrl: './header.component.html',
+  imports: [RouterLink, ProfileComponent, RouterOutlet],
+})
+export class HeaderComponent {
+  name = 'LuckyJob';
+
+  navLinks = [
+    { id: 1, title: 'Find job', link: '' },
+    { id: 2, title: 'Messages', link: '' },
+    { id: 3, title: 'Hiring', link: '' },
+    { id: 4, title: 'Community', link: '' },
+    { id: 5, title: 'FAQ', link: '' },
+  ];
+
+  linkClass =
+    'text-sm text-custom-grey-1 hover:opacity-85 duration-150 py-5 border-b-[2px]';
+
+  location = 'New York, NY';
+
+  currentPage = this.navLinks[0].title;
+
+  toggleNav(page: string) {
+    this.currentPage = page;
+  }
+}
