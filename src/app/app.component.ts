a877ca90f1cdb09b5a3015b181a43ac5194acaaf
ikeawesom <ikelimjl@gmail.com>
2024-10-19 14:06:21 +0800

    removed styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { HeaderComponent } from '../header/header.component';
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
   imports: [RouterOutlet, HeaderComponent],
 })
 export class AppComponent {
