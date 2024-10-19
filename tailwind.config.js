efecdb7ad27eef90781253c527fab01daf2437a7
ikeawesom <ikelimjl@gmail.com>
2024-10-19 16:15:13 +0800

    updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
       colors: {
         "custom-primary": "#97CBFA",
         "custom-black": "#14140F",
-        "custom-grey-3": "#616366",
-        "custom-grey-2": "#C6C6CA",
-        "custom-grey-1": "#C2C2C2",
+        "custom-grey-3": "#414345",
+        "custom-grey-2": "#A6A6A6",
+        "custom-grey-1": "#C6C6CA",
         "custom-white-1": "#F5F5F5",
       },
     },
