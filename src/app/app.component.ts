ca7dd739783264707ad12387530d55328b442c66
ikeawesom <ikelimjl@gmail.com>
2024-10-19 16:55:51 +0800

    added job page section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { HeaderFilterComponent } from '../filtering/header/header-filter.component';
 import { SideFilter } from '../filtering/side/side-filter.component';
+import { JobPage } from '../jobs/job-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, HeaderComponent, HeaderFilterComponent, SideFilter],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeaderFilterComponent,
+    SideFilter,
+    JobPage,
+  ],
 })
 export class AppComponent {
   title = 'marketplace';
