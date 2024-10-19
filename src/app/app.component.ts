86335044ddf8bbcf266c5c2cf8aa9a8fa2bee206
ikeawesom <ikelimjl@gmail.com>
2024-10-19 16:17:23 +0800

    added side bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { HeaderFilterComponent } from '../filtering/header/header-filter.component';
+import { SideFilter } from '../filtering/side/side-filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, HeaderComponent, HeaderFilterComponent],
+  imports: [RouterOutlet, HeaderComponent, HeaderFilterComponent, SideFilter],
 })
 export class AppComponent {
   title = 'marketplace';
