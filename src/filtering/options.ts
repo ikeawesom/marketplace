7d193857d28b1151ea103642df8e7257b2a7a885
ikeawesom <ikelimjl@gmail.com>
2024-10-19 16:16:54 +0800

    extracted constants

---
+++ src/filtering/options.ts
@@ -0,0 +1,15 @@
+export const WORK_OPTIONS = [
+  'Full time',
+  'Part time',
+  'Internship',
+  'Project work',
+  'Volunteering',
+];
+
+export const EXPLOYMENT_TYPES = [
+  'Full day',
+  'Flexible schedule',
+  'Shift work',
+  'Distant work',
+  'Shift method',
+];
